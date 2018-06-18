0c74ef018d5d411656ca60d9b161e72da3b34d62
Abhishek Agarwal <aagarwal440@gmail.com>
2018-06-18 10:31:05 +0530

    Phone Numner MOdel Update

--- party/models.py
+++ party/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.core.validators import RegexValidator
 # Create your models here.
 
 class party_Ledger(models.Model):
@@ -10,7 +11,8 @@ class party_Ledger(models.Model):
     Name = models.CharField(max_length=200, null=False)
     Address = models.TextField(max_length=200, null=True)
     gstin = models.CharField('GSTIN',max_length=15, null=False,unique=True)
-    Contact = models.IntegerField(null=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    Contact = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     def __str__(self):
         """
         String for representing the Model object.
